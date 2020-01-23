164c7fe6c1e43e46a979a218ffc0743a16e12d9b
Sergiu Zagoret <szagoret@complexica.com>
2020-01-23 14:15:04 +0200

    Create aggregation file for all pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React, {Suspense} from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.scss';
 import ResponsiveContainer from "./containers/ResponsiveContainer";
-import NotesPage from "./pages/NotesPage";
 import {Provider} from "react-redux";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {history} from './utils/history';
 import store from './store/store';
-import TheoryPage from "./pages/TheoryPage";
-import PhotosPage from "./pages/PhotosPage";
-import EventsPage from "./pages/EventsPage";
-import AudioPage from "./pages/AudioPage";
+import {AudioPage, EventsPage, NotesPage, PhotosPage, TheoryPage} from './pages';
 
 export default () => {
     return (
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import NotesPage from "./NotesPage";
+import TheoryPage from "./TheoryPage";
+import PhotosPage from "./PhotosPage";
+import EventsPage from "./EventsPage";
+import AudioPage from "./AudioPage";
+
+
+export {NotesPage, TheoryPage, PhotosPage, EventsPage, AudioPage};
