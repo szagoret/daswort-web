0ad5b876b85bc0c731d6211eec09e27b3645ea2d
Sergiu Zagoret <szagoret@complexica.com>
2020-01-28 17:08:04 +0200

    Adjust search result style

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Label, Search} from "semantic-ui-react";
+import {Breadcrumb, Container, Search, Segment} from "semantic-ui-react";
 import {findSongsByNameApi} from "../api/NotesApi";
 import {debounce} from 'lodash';
 import {history} from '../utils/history';
@@ -7,54 +7,78 @@ import {isNotEmpty} from "../utils/StringUtils";
 
 const SongSearch = () => {
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [result, setResult] = useState([]);
+        const [isLoading, setIsLoading] = useState(false);
+        const [searchTerm, setSearchTerm] = useState('');
+        const [result, setResult] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            const {data} = await findSongsByNameApi(searchTerm);
-            setResult(data);
-            setIsLoading(false);
+        useEffect(() => {
+            const fetchData = async () => {
+                setIsLoading(true);
+                const {data} = await findSongsByNameApi(searchTerm);
+                setResult(data);
+                setIsLoading(false);
+            };
+            if (isNotEmpty(searchTerm)) {
+                fetchData();
+            }
+        }, [searchTerm]);
+
+
+        const handleOnResultSelect = (e, {result}) => {
+            setSearchTerm('');
+            history.push(`/song/${result.song.id}`);
         };
-        if (isNotEmpty(searchTerm)) {
-            fetchData();
-        }
-    }, [searchTerm]);
+
+        const handleSearchChange = (e, {value}) => {
+            debounce(setSearchTerm, 1000, {leading: true,})
+            setSearchTerm(value);
+        };
 
 
-    const handleOnResultSelect = (e, {result}) => {
-        setSearchTerm('');
-        history.push(`/song/${result.song.id}`);
-    };
+        const resultRenderer = ({path, song}) => {
+            const sections = path.map((item, i) => {
+                return {key: item.id, content: item.name}
+            });
+            return (
+                <Container>
+                    <Segment basic style={{
+                        margin: '3px',
+                        padding: '.5em .5em',
+                        color: 'rgba(0,0,0,.4)',
+                        fontWeight: '400',
+                        fontSize: '1em',
+                        fontStyle: 'normal'
+                    }}>
+                        <Breadcrumb sections={sections}/>
+                    </Segment>
+                    <Segment basic style={{
+                        // backgroundColor: 'rgb(250, 252, 255)',
+                        margin: '3px',
+                        padding: '.5em .5em'
+                    }}>
+                        {song.name}
+                    </Segment>
+                </Container>
+            );
+        };
 
-    const handleSearchChange = (e, {value}) => {
-        setSearchTerm(value);
-    };
-
-
-    const resultRenderer = (props) => {
-        console.log('am ajuns');
-        return (<Label content={props.song.name}/>);
-    };
-
-    return (
-        <>
-            <Search
-                className="main-search"
-                fluid
-                input={{fluid: true}}
-                loading={isLoading}
-                onResultSelect={handleOnResultSelect}
-                onSearchChange={debounce(handleSearchChange, 1000, {leading: true,})}
-                resultRenderer={resultRenderer}
-                results={result}
-                value={searchTerm}
-            />
-        </>
-    );
-};
+        return (
+            <>
+                <Search
+                    className="main-search"
+                    fluid
+                    input={{fluid: true}}
+                    loading={isLoading}
+                    onResultSelect={handleOnResultSelect}
+                    onSearchChange={debounce(handleSearchChange, 1000, {leading: true,})}
+                    resultRenderer={resultRenderer}
+                    results={result}
+                    value={searchTerm}
+                />
+            </>
+        );
+    }
+;
 
 
 export default SongSearch;
